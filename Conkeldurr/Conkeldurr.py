0bca9043ab20ed0443a0aea9afc30075fef207a8
ZMC_home01 <ZMC_home01@DESKTOP-UAUC6CG>
2023-12-25 07:10:38 +0800

    完成回测初版

--- Conkeldurr/Conkeldurr.py
+++ Conkeldurr/Conkeldurr.py
@@ -15,6 +15,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.metrics import median_absolute_error,mean_absolute_percentage_error
 
+import warnings
+warnings.filterwarnings('ignore')
+
 def downloadall(pro):
 
     #df_calender = pro.trade_cal(exchange='SHFE', start_date='20230801', end_date='20231031')
@@ -119,11 +122,11 @@ def get_ETF_info(pro):
     #df = pro.fund_basic(market='E')
     #df.to_csv("seee3.csv",encoding='utf-8-sig')
 
-    df50etf = pro.fund_daily(ts_code='510050.SH', start_date='20200101', end_date='20231101')
-    df300etf = pro.fund_daily(ts_code='510300.SH', start_date='20200101', end_date='20231101')
-    df500etf = pro.fund_daily(ts_code='510500.SH', start_date='20200101', end_date='20231101')
-    dfKCetf = pro.fund_daily(ts_code='588000.SH', start_date='20200101', end_date='20231101')
-    dfCYBetf = pro.fund_daily(ts_code='159915.SZ', start_date='20200101', end_date='20231101')
+    df50etf = pro.fund_daily(ts_code='510050.SH', start_date='20200101', end_date='20231111')
+    df300etf = pro.fund_daily(ts_code='510300.SH', start_date='20200101', end_date='20231111')
+    df500etf = pro.fund_daily(ts_code='510500.SH', start_date='20200101', end_date='20231111')
+    dfKCetf = pro.fund_daily(ts_code='588000.SH', start_date='20200101', end_date='20231111')
+    dfCYBetf = pro.fund_daily(ts_code='159915.SZ', start_date='20200101', end_date='20231111')
 
     print(df50etf)
     print(df300etf)
@@ -160,8 +163,6 @@ def regression_metrics(true,pred):
     #print('绝对百分比误差中位数【MedianAPE】:',median_absolute_percentage_error(true,pred))
 
 def from_chatgpt():
-
-    
 
     # 读入数据
     df = pd.read_csv('opt_see1129.csv', encoding='gbk')
@@ -234,10 +235,96 @@ def from_chatgpt():
     regression_metrics(y_test, y_pred)
     xxxx=1
 
+def train_option_exercise_probability_model():
+
+    df=pd.read_csv('try1224.csv',index_col=0,header=0,encoding='utf-8-sig')
+
+    df['ETF_close'].fillna(0, inplace=True)
+    df['close'].fillna(0, inplace=True)
+    df['pre_close'].fillna(0, inplace=True)
+    df['pre_close'].fillna(0, inplace=True)
+
+    # # 划分验证集
+    # valid = df.sample(frac=0.2, random_state=42)
+    # df.drop(index=valid.index, axis=1, inplace=True)
+
+    print(df)
+
+    mapping_call_put = {'C': 1, 'P': 2}
+    
+    mapping_opt_code = {'510050.SH': 1, '510300.SH': 2, '510500.SH': 3, '588000.SH': 4, '588080.SH': 5}
+
+    df['call_put'] = df['call_put'].map(mapping_call_put)
+    df['opt_code'] = df['opt_code'].map(mapping_opt_code)
+
+    print(df)
+    dfindex=df
+    df = df.drop(['ts_code'],axis=1,inplace=False)
+
+    #df=df[['pre_close','opt_code','call_put','days_remain','Exercise_Status']]
+
+
+    # 划分训练集和测试集
+    
+    X = df[['pre_close','close','ETF_close','real_value','opt_code','call_put','days_remain']]
+    y = df['Exercise_Status']
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    train_ids = X.index.tolist()
+    splitno=int(len(train_ids)*0.30)
+    
+    X_train = X.iloc[:splitno, :]
+    X_test=X.iloc[(splitno+100):, :]
+    
+    y_train=y[:splitno]
+    y_test=y[(splitno+100):]
+
+    dfindex_train=dfindex.iloc[:splitno, :]
+    dfindex_test=dfindex.iloc[(splitno+100):, :]
+    
+    print(X_train)
+    print(dfindex_test)
+
+    # 创建LightGBM数据集
+    train_data = lgb.Dataset(X_train, label=y_train)
+    val_data = lgb.Dataset(X_test, label=y_test, reference=train_data)
+
+    # 设置参数
+    params = {
+        'objective': 'binary',  # 二分类任务
+        'metric': 'binary_logloss',  # 使用对数损失作为评估指标
+        'boosting_type': 'gbdt',  # 使用梯度提升树
+        'num_leaves': 31,  # 叶子节点数
+        'learning_rate': 0.05,  # 学习率
+        'feature_fraction': 0.9,  # 特征采样比例
+        'bagging_fraction': 0.8,  # 数据采样比例
+        'bagging_freq': 5,  # 数据采样频率
+        'verbose': 0  # 控制输出信息
+    }
+
+    # 训练模型
+    model = lgb.train(params, train_data, valid_sets=[val_data], num_boost_round=100)
+
+    # 预测
+    predictions = model.predict(X_test)
+
+    outputy=pd.DataFrame(predictions,columns=['y_pred'])
+
+    dfindex_test.reset_index(inplace=True,drop=True)
+    
+    print(dfindex_test)
+
+    dfindex_test['Exercise_pred']=outputy
+
+    print(dfindex_test)
+    
+    dfindex_test.to_csv('see1224.csv')
+
+    zzzz=1
 
 def opt_backTesting():
 
-    daily_all_df=pd.read_csv('try1216.csv',header=0,encoding='utf-8-sig')
+    daily_all_df=pd.read_csv('see1224.csv',header=0,index_col=0,encoding='utf-8-sig')
 
     print(daily_all_df)
 
@@ -252,17 +339,33 @@ def opt_backTesting():
     baselinerd=1
     
     hold_list=[]
+    hold_list=pd.DataFrame(columns=('ts_code','call_put','buy_amount','lastprice'))
+    
+    account=10000000
+    buy_pct=0.5
+    accountbase=account
+    lastallvalue=account
+    #当日结束总资产
+    allvalue=account
+
+    buyorsell=0
 
     seed_value = 66
     np.random.seed(seed_value)
 
+    # skip1=0
     for cur_date in datelist:
-        
+        # skip1+=1;
+        # if(skip1<2):
+        #     continue;
+
         #获取当日的数据
         cur_df_all=daily_all_df[daily_all_df['trade_date'].isin([cur_date])]
-        cur_df_all.sort_values(by='exercise_price', ascending=True, inplace=True)
-        
-        filtered_df = cur_df_all.loc[cur_df_all['opt_code'] == '510300.SH']
+        cur_df_all=cur_df_all.sort_values(by='exercise_price', ascending=True)
+
+        #这里改成代号
+        # mapping_opt_code = {'510050.SH': 1, '510300.SH': 2, '510500.SH': 3, '588000.SH': 4, '588080.SH': 5}
+        filtered_df = cur_df_all.loc[cur_df_all['opt_code'] == 2]
         if(len(filtered_df)==0):
             curpctchg=0
         else:
@@ -271,29 +374,89 @@ def opt_backTesting():
         baseline=baseline*(1+curpctchg/100)
         baseline50.append(baseline)
 
-        print(cur_df_all)
+
+        # 计算新的价值，卖出hold的期权，计算资产
+        if hold_list.shape[0]>0 :
+            hold_list_buffer=pd.merge(hold_list,filtered_df, how='left', on=['ts_code'])
+            hold_list_buffer.reset_index(inplace=True,drop=True)
+            #print(hold_list_buffer)
+
+            hold_list.loc[:,'lastprice']=hold_list_buffer['close']
+            
+            #print(hold_list)
+            dsfsdf=1
+
+        #卖出逻辑
+        if hold_list.shape[0]>0 :
+            #简单的直接清空
+            hold_list_value=hold_list['buy_amount']*hold_list['lastprice']*10000
+            #前面买入这里加
+            if buyorsell == 0 :          
+                account=account+hold_list_value.sum()
+            else:
+                account=account-hold_list_value.sum()
+            hold_list=pd.DataFrame(columns=('ts_code','call_put','buy_amount','lastprice'))
+            allvalue=account
+            dsfsdf=1
+            
 
         #策略一 简单的买入所有认购
 
-        buylist=cur_df_all    
+        buy_all_value=allvalue*buy_pct
+
+        if(cur_date>20231100):
+            print(filtered_df)
+            
+        buylist=filtered_df    
         buylist=buylist.sort_values(by=['close'])
-        buylist=buylist[buylist['call_put']=="C"]
-        buylist=buylist[buylist['close']>0.02]
-        buylist=buylist[buylist['days_remain']<30]
+        #这里改成代号
+        # mapping_call_put = {'C': 1, 'P': 2}
+        buylist=buylist[buylist['call_put']==1]
+        buylist=buylist[buylist['close']>0.05]
+        buylist=buylist[buylist['days_remain']<50]
+        buylist=buylist[buylist['days_remain']>5]
 
-        buylist=buylist.head(3)
+        buylist=buylist.head(1)
+        
+        if(cur_date>20231100):
+            print(buylist)
+            
+        #根据buylist做后期计算
+        buylist.loc[:,'buyuse']=buy_all_value/(buylist['ETF_close']*10000*buylist['Exercise_pred'])
 
-        print(buylist)
+        #buylist['buyuse']=code_amount_buy/buylist['close']
+        buylist.loc[:,'buyuse']=buylist['buyuse'].round(0)
+        buylist.loc[:,'buyuse']=buylist['buyuse'].astype(int)
+        buylist['value']=buylist['close']*buylist['buyuse']*10000
+        #print(buylist)
 
-        savebuylist=buylist[['ts_code','open','call_put','close']]
-        savebuylist.columns = ['ts_code','open','call_put','buy_price']
+        savebuylist=buylist[['ts_code','call_put','buyuse','close']]
+        savebuylist.columns = ['ts_code','call_put','buy_amount','lastprice']
         savebuylist['last_action_flag']=0
-
+        
+        if buyorsell == 0 :          
+            account=account-buylist['value'].sum()-buylist['buyuse'].sum()*2
+        else:
+            account=account+buylist['value'].sum()-buylist['buyuse'].sum()*2
         hold_list=hold_list.append(savebuylist)
+        hold_list.reset_index(inplace=True,drop=True)
+
+        #计算当前总资产
+        hold_list_value=hold_list['buy_amount']*hold_list['lastprice']*10000
+        #这里当卖出        
+        if buyorsell == 0 :          
+            allvalue=hold_list_value.sum()+account
+        else:
+            allvalue=-hold_list_value.sum()+account
 
         single_random_number = np.random.normal(mean, std_dev, size=1)
         
-        baselinerd=baselinerd*(1+single_random_number/100)
+        #baselinerd=baselinerd*(1+single_random_number/100)
+
+        baselinerd=allvalue/accountbase
+        print(allvalue)
+        print(cur_date)        
+
         show3.append(baselinerd)
 
 
@@ -366,8 +529,22 @@ def opt_datemerge():
 
     print(daily_all_df)
 
-    daily_all_df.to_csv('try1216.csv',encoding='utf-8-sig')
+    daily_all_df=daily_all_df.sort_values(by='trade_date')
+    last_values = daily_all_df.groupby('ts_code')['close'].last().reset_index()
 
+    last_values.rename(columns={'close': 'last_close'}, inplace=True) 
+
+    daily_all_df = pd.merge(daily_all_df,last_values,on='ts_code',how='left')
+    
+    daily_all_df['Exercise_Status']=0
+    daily_all_df.loc[daily_all_df['last_close'] > 0.005, 'Exercise_Status'] = 1
+
+    daily_all_df['real_value']=daily_all_df['ETF_close']-daily_all_df['exercise_price']
+
+
+    daily_all_df.to_csv('try1224.csv',encoding='utf-8-sig')
+
+    ddd=1
 
 if __name__ == '__main__':
 
@@ -389,9 +566,11 @@ if __name__ == '__main__':
     #downloadall(pro)
     #opt_datemerge()
 
+    #train_option_exercise_probability_model()
+    
     opt_backTesting()
 
-    from_chatgpt()
+    #from_chatgpt()
 
     df_opt_basic_sse=pd.read_csv('tushare_opt_basic_1126.csv',index_col=0,header=0,encoding='utf-8-sig')
 
